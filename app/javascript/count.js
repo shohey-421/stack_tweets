ea478f52fd0e884565b60ccb8fdfdab63648ed82
shohey-421 <shohey-421@users.noreply.github.com>
2020-09-26 19:43:05 +0900

    累計機能の追加

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,15 +1,21 @@
 $(document).on('turbolinks:load', function() {
 
-  function createHTML(stack,Count) {
-    let stackTweet = `・${stack.task} ${Count}${stack.unit}<br>`
-    return stackTweet;
+  function createHTML(stack,check,Count) {
+    if( check ){
+      let stackTweet = `・${stack.task} ${stack.count}${stack.unit}<br>`
+      return stackTweet;
+    } else { 
+      let stackTweet = `・${stack.task} ${Count}${stack.unit}<br>`
+      return stackTweet;
+    }
   }
 
   $('.count-form').each(function(){
     $(this).on("submit", function(e) {
       e.preventDefault(); 
       let index = $(this).children('.stack-index').text()
-      let inputCount = $(`#today-stack-field-${index}`).val(); 
+      let inputCount = $(`#today-stack-field-${index}`).val();
+      let sumCheck = $(`#sum-check-${index}`).prop('checked')
       let url = $(this).attr("action"); 
       $.ajax({
         url: url,  
@@ -20,7 +26,7 @@ $(document).on('turbolinks:load', function() {
         dataType: "json"  
       })
       .done(function(data) {
-        let stackTweet = createHTML(data,inputCount); 
+        let stackTweet = createHTML(data,sumCheck,inputCount); 
         $(".tweet-sample").append(stackTweet); 
         $(`#stack-count-${index}`).text(data.count + data.unit)
         $(`#today-stack-field-${index}`).val("")  
