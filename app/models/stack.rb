ce3a1696e694da7e8ae0cf5660cfa1987f4f12bc
shohey-421 <shohey-421@users.noreply.github.com>
2020-09-27 21:26:28 +0900

    バリデーション修正

--- app/models/stack.rb
+++ app/models/stack.rb
@@ -1,7 +1,7 @@
 class Stack < ApplicationRecord
   belongs_to :user
 
-  validates :task, presence:true
-  validates :unit, presence:true
+  validates :task, presence:true, length: { maximum: 15 }
+  validates :unit, presence:true, length: { maximum: 5 }
   validates :count, numericality: {only_integer: true}
 end
