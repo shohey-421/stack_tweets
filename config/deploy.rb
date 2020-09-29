c48c900a5c53286e90192744914e38be14304978
shohey-421 <shohey-421@users.noreply.github.com>
2020-09-29 14:53:13 +0900

    自動デプロイエラー修正5

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'stack_tweets'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:shohey-421/stack-tweets.git'
+set :repo_url,  'git@github.com:shohey-421/stack_tweets.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
