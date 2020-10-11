7c494ada839c26b4e5c035da98b37ad74a420497
shohey-421 <shohey-421@users.noreply.github.com>
2020-10-11 20:15:47 +0900

    ツイート文内url短縮

--- app/javascript/tweet.js
+++ app/javascript/tweet.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function(){
   $('#tweet-btn').on('click',function(){
     const originalText = $('.tweet-sample').html().trim();
     const tweetText = originalText.replace(/#/g,'%23').replace(/<br>/g,'%0a') + '%0a'
-    const site_url = "https://stack-tweets.heys-apps.work/"
+    const site_url = "https://bit.ly/36S5ZRs"
     open( `https://twitter.com/intent/tweet?text=${tweetText}&url=${site_url}`, "_blank") ;
     $('.tweet-sample').text("#今日の積み上げ");
   });
