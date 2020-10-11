fbe5c76ee5786cb22c422f53be9adaa4806101de
shohey-421 <shohey-421@users.noreply.github.com>
2020-10-11 20:18:27 +0900

    ツイート後の本文エリア修正

--- app/javascript/tweet.js
+++ app/javascript/tweet.js
@@ -4,6 +4,6 @@ $(document).on('turbolinks:load', function(){
     const tweetText = originalText.replace(/#/g,'%23').replace(/<br>/g,'%0a') + '%0a'
     const site_url = "https://bit.ly/36S5ZRs"
     open( `https://twitter.com/intent/tweet?text=${tweetText}&url=${site_url}`, "_blank") ;
-    $('.tweet-sample').text("#今日の積み上げ");
+    $('.tweet-sample').html("#今日の積み上げ<br>");
   });
 });
